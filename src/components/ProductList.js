fcb193010726539a2930648bfdff02e3e4c177f0
Dinesh Mood <dineshmood95@gmail.com>
2025-10-24 18:57:49 -0500

    table and searchbox css updates

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,51 +1,42 @@
-
-import React  from "react";
+import React from "react";
 import ProductCard from "./ProductCard";
-import "../css/ProductList.css"
+import "../css/ProductList.css";
 
-function ProductList({products, favourites, onToggleFavourite}) {
-    return (
-   
-         <div className="product-list">
-
-            <table className="product-table">
-                <thead className="table-head">
-                    <th>
-                        ID
-                    </th>
-                     <th>
-                        TITLE
-                    </th>
-                     <th>
-                        BODY
-                    </th>
-                     <th>
-                        Action
-                    </th>
-                </thead>
-                <tbody>
-
-         {products.map((product)=> {
-                
-         const isFavourite = favourites.includes(product.id);
-               return (    <tr className="product-card">
+function ProductList({ products, favourites, onToggleFavourite }) {
+  return (
+    <div className="product-list">
+      <table className="product-table">
+        <thead className="table-head">
+          <th>ID</th>
+          <th>TITLE</th>
+          <th>BODY</th>
+          <th>Action</th>
+        </thead>
+        <tbody>
+          {products.map((product) => {
+            const isFavourite = favourites.includes(product.id);
+            return (
+              <tr className="product-card">
                 <td>{product.id}</td>
-            <td>{product.title}</td>
-             <td>{product.body}</td>
+                <td>{product.title}</td>
+                <td>{product.body}</td>
 
-               <td> <button onClick={()=>onToggleFavourite(product.id)}>
-{isFavourite ? "**Favourite" : "Mark Favourite"}
-             </button></td>
-
-        </tr>)
-})}
-
-                </tbody>
-            </table>
-
-         </div>
-    );
+                <td>
+                  {" "}
+                  <button
+                    className="favourite-btn"
+                    onClick={() => onToggleFavourite(product.id)}
+                  >
+                    {isFavourite ? "**Favourite" : "Mark Favourite"}
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
-
 
 export default ProductList;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,36 +1,30 @@
-import React, { useEffect, useState }  from "react";
+import React, { useEffect, useState } from "react";
+import "../css/SearchBar.css";
 
-function SearchBar({value, onChange}) {
+function SearchBar({ value, onChange }) {
+  const [searchValue, setSearchValue] = useState(value);
 
-    const [searchValue, setSearchValue] = useState(value);
+  const handleChange = (e) => setSearchValue(e.target.value);
 
-    const handleChange = (e) => setSearchValue(e.target.value);
-
-    useEffect(() => {
-
-     const timeoutId = setTimeout(() => {
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
       onChange(searchValue);
     }, 500);
 
-
     return () => clearTimeout(timeoutId);
+  }, [searchValue, onChange]);
 
-    }, [searchValue, onChange]);
-
-    
-
-    return(
-        <div>
-
-            <input
-            
-             type="text"
-             placeholder="Search products"
-             value={searchValue}
-             onChange={handleChange}
-            />
-        </div>
-    )
+  return (
+    <div className="searchbar">
+      <input
+        type="text"
+        className="searchbar-input"
+        placeholder="Search products"
+        value={searchValue}
+        onChange={handleChange}
+      />
+    </div>
+  );
 }
 
 export default SearchBar;
