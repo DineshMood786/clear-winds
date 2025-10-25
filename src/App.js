234b537d82d56b6d358a6ba9db237fe6a2b0883b
Dinesh Mood <dineshmood95@gmail.com>
2025-10-24 19:18:02 -0500

    sorted data

--- src/App.js
+++ src/App.js
@@ -1,73 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
-import ProductList from './components/ProductList';
-import SearchBar from './components/SearchBar';
-import Loader from './components/Loader';
-import ErrorMessage from './components/ErrorMessage';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import ProductList from "./components/ProductList";
+import SearchBar from "./components/SearchBar";
+import Loader from "./components/Loader";
+import ErrorMessage from "./components/ErrorMessage";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filterd, setFiltered] = useState([]);
 
-const [products,setProducts] = useState([]);
-const [searchTerm, setSearchTerm] = useState("");
-const [filterd,setFiltered] = useState([]);
+  const toggleFavorite = (id) => {
+    console.log(favourites);
+    setFavourites((prev) =>
+      prev.includes(id) ? prev.filter((f) => f !== id) : [...prev, id]
+    );
+  };
+  const [favourites, setFavourites] = useState([]);
 
+  const [loading, setLoading] = useState(true);
 
-const toggleFavorite = (id) => {
-  console.log(favourites);
-  setFavourites((prev)  => prev.includes(id) ? prev.filter((f) => f!==id) : [...prev, id])
-}
-const [favourites,setFavourites] = useState([]);
+  const [error, setError] = useState(null);
 
-const [loading, setLoading] =useState(true);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("failed to fetch products");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        const sortedData = data.sort((a, b) => a.title.localeCompare(b.title));
 
-const [error, setError] =useState(null);
+        setProducts(sortedData);
+        setLoading(false);
+        setFiltered(sortedData);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(err.message);
+      });
+  }, []);
 
-useEffect(() => {
-  fetch("https://jsonplaceholder.typicode.com/posts")
-  .then((res) => {
-     if(!res.ok) {
-      throw new Error('failed to fetch products');
-     }
-     return res.json();
-  }).then (data => {
+  useEffect(() => {
+    setLoading(true);
+    const lower = searchTerm?.toLowerCase();
 
-    setProducts(data);
-    setLoading(false)
-    setFiltered(data)
-  }) .catch((err)=> {
-  
-    setLoading(false)
-    setError(err.message)
-  });
-}, []);
+    const result = products.filter(
+      (p) =>
+        !searchTerm ||
+        p.title.toLowerCase().includes(lower) ||
+        p.body.toLowerCase().includes(lower)
+    );
 
-useEffect(()=> {
+    setFiltered(result);
+    setLoading(false);
+  }, [searchTerm, products]);
 
-  setLoading(true)
-  const lower = searchTerm?.toLowerCase();
+  if (loading) {
+    return <Loader />;
+  }
 
-  const result = products.filter(
-
-    (p)=> !searchTerm || p.title.toLowerCase().includes(lower) || p.body.toLowerCase().includes(lower)
-  )
-
-  setFiltered(result);
-  setLoading(false)
-  
-}, [searchTerm, products])
-
-if(loading) {
-return <Loader/>
-}
-
-if(error) return <ErrorMessage message={error} />
+  if (error) return <ErrorMessage message={error} />;
 
   return (
     <div className="App">
-      <SearchBar  
-      value={searchTerm} onChange={setSearchTerm}/> 
-         <ProductList products={filterd} favourites={favourites} onToggleFavourite={toggleFavorite}/>
+      <SearchBar value={searchTerm} onChange={setSearchTerm} />
+      <ProductList
+        products={filterd}
+        favourites={favourites}
+        onToggleFavourite={toggleFavorite}
+      />
     </div>
   );
 }
